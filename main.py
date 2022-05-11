0e6b7d77dbf51d2c9fef24c939cee5aea7fcba5b
2Changele2 <mariad.a.hidalgo@gmail.com>
2022-05-11 13:15:58 +0200

    Multiples balas
    Múltiples balas en direcciones en linea 9

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 
 
+
+
+
 def punto_de_mira(lista_de_coordenadas):
     for coordenadas in lista_de_coordenadas:
-        plt.plot ([0],[1], 'bo')
+        plt.plot ([2, 2, 0, 4], [1, 16, 9, 9],'bo')
 
-    plt.plot(0, 1, 'k+')
+        
+    
+    plt.plot(2, 9, 'k+')
     plt.ylabel('Y')
     plt.xlabel('X')
     plt.show()
 
 
 if __name__ == '__main__':
-    lista_de_coordenadas = [0, 0]
+    lista_de_coordenadas = [ 0, 1 ]
     punto_de_mira(lista_de_coordenadas)
